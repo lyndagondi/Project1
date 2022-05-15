130156b8b004a3f2dd41559b37214c75b6cfb929
lyndagondi <83711086+lyndagondi@users.noreply.github.com>
2022-05-15 12:11:53 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIA3CJ6S4UBX6B2MAZT"
-  secret_key = "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"
+  access_key = "awsaccesskey"
+  secret_key = "awssecretkey"
 }
 resource "aws_instance" "terraEC2" {
   ami           = "ami-0aeb7c931a5a61206"
