a27009f726ed1d710b6a35ce9cb4667216ddfa09
lyndagondi <83711086+lyndagondi@users.noreply.github.com>
2022-05-16 23:02:29 +0530

    Update aws.tf
    keypair resource attached

--- aws.tf
+++ aws.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-2"
+  region     = "ap-south-1"
   access_key = "awsaccesskey"
   secret_key = "awssecretkey"
 }
@@ -9,4 +9,9 @@ resource "aws_instance" "terraEC2" {
   tags = {
     Name = "firstec2"
   }
+  key_name   = "iackp"
+}
+resource "aws_key_pair" "terraform-keypair" {
+  key_name   = "iackp"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLQVsM9XAP28A8VoK8NZmguIaL9zq0cbNn3Qw9A8I9TQrbmx3giY5OqWt5r477SWBUy6J0UveO6ItkBLu8DrON0AGToJQVI2O//dJKjUC469YpdB2xLHUL0Ys+kauEb9/M4meZ/3S5lFFsiyU2a0m1Pe1lIez6shIBwvlCkJTKDi1HIlJ01LbeWaXnSA/2NWbiRpoHZQJqYOfVgQfICTA4WgIwYcjfeYlhSCZaWQcAIsXC1BVDZZ6/uFVY+an3cxal5+xr282ZJIHpo9dpPPEjGa2uc+ol7Y1IuzLJZE4tzYyj53lLS+7y3Z/hLpJYzzn1Evut+LBkKFIah+TcIldx root@ip-172-31-0-21.ap-south-1.compute.internal"
 }
