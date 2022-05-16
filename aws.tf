665016dc0b255f8138333e2d9e1470e7d881d9f0
lyndagondi <83711086+lyndagondi@users.noreply.github.com>
2022-05-16 23:25:08 +0530

    Update aws.tf
    valid AMI given

--- aws.tf
+++ aws.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "awssecretkey"
 }
 resource "aws_instance" "terraEC2" {
-  ami           = "ami-0aeb7c931a5a61206"
+  ami           = "ami-079b5e5b3971bd10d"
   instance_type = "t2.micro"
   tags = {
     Name = "firstec2"
