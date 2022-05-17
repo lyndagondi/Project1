3a1a448577e8e24f65ec7a7d2e64e95492ef6afc
lyndagondi <83711086+lyndagondi@users.noreply.github.com>
2022-05-17 10:22:34 +0530

    Update aws.tf
    keypair changes

--- aws.tf
+++ aws.tf
@@ -6,12 +6,12 @@ provider "aws" {
 resource "aws_instance" "terraEC2" {
   ami           = "ami-079b5e5b3971bd10d"
   instance_type = "t2.micro"
-  key_name      = "iackp"
+  key_name      = "terraformkp"
   tags = {
     Name = "firstec2"
   }
 }
 resource "aws_key_pair" "terraformkp" {
-  key_name   = "iackp"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLQVsM9XAP28A8VoK8NZmguIaL9zq0cbNn3Qw9A8I9TQrbmx3giY5OqWt5r477SWBUy6J0UveO6ItkBLu8DrON0AGToJQVI2O//dJKjUC469YpdB2xLHUL0Ys+kauEb9/M4meZ/3S5lFFsiyU2a0m1Pe1lIez6shIBwvlCkJTKDi1HIlJ01LbeWaXnSA/2NWbiRpoHZQJqYOfVgQfICTA4WgIwYcjfeYlhSCZaWQcAIsXC1BVDZZ6/uFVY+an3cxal5+xr282ZJIHpo9dpPPEjGa2uc+ol7Y1IuzLJZE4tzYyj53lLS+7y3Z/hLpJYzzn1Evut+LBkKFIah+TcIldx root@ip-172-31-0-21.ap-south-1.compute.internal"
+  key_name   = "terraformkp"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCk8w1SplfKIcVHdM+Ubh/XGpwiqsFmGMwHl3j39tBVQR43AbUPE5n/B/2EAOigAearX2QUJdmbNFP5147FNP2mRVpk8VrzL+gRQqvQG0sfJl0rJrxQFX+KNuz9asdy5Wfz//U1jj0YNp9iKs7Pc8Jm8h9vbpN5/iFM8m1B/i9ebdpa3MZ+8H1O7qw43CVhrsALLiqu0Ug0AZ8oq87rdnhH+VzEacWFaJNWv/jr5ZXqhjpXtquaeMGz8wkqdFV/hztH7cPNaJhDOjyuRf6JPVx4Q6roN991Zik1MdYfHuvcoJ+HgM95y8KEFFojj6JknpudyjyvbasA3kmmHLR6Eh+7 root@ip-172-31-0-21.ap-south-1.compute.internal"
 }
