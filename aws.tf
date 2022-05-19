638fa6fc3ab6aecfc6a9f0a042aee9891f3f9ba1
root <root@ip-172-31-5-251.ap-south-1.compute.internal>
2022-05-19 06:19:08 +0000

    na-instance

--- aws.tf
+++ aws.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "terraEC2" {
   ami           = "ami-0aeb7c931a5a61206"
   instance_type = "t2.micro"
   tags = {
-    Name = "terraform-instance"
+    Name = "na-instance"
   }
 }
