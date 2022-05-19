d868a61235ac44b0a6bd571607515bd176f6a28b
root <root@ip-172-31-5-251.ap-south-1.compute.internal>
2022-05-19 06:01:51 +0000

    instance name changed

--- aws.tf
+++ aws.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "terraEC2" {
   ami           = "ami-0aeb7c931a5a61206"
   instance_type = "t2.micro"
   tags = {
-    Name = "firstec2"
+    Name = "terraform-instance"
   }
 }
